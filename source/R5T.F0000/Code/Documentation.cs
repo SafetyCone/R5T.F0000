f17df96bb9b073e10e8cb6bad64391f508eaa883
davidcoats <david@getsafetycone.com>
2022-05-03 13:23:38 -0700

    Initial commit of source-stub.

---
+++ source/R5T.F0000/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.F0000
+{
+    /// <summary>
+    /// Common operations on .NET C# built-in types, like characters, strings, integers, etc.
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
