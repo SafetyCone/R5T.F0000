d2ce6145cbc9fe1bd846da142653a4ffbcd55fc8
davidcoats <david@getsafetycone.com>
2026-04-07 12:29:05 -0700

    Interim commit.

--- source/R5T.F0000/Code/Functionality/IWasFoundOperator.cs
+++ source/R5T.F0000/Code/Functionality/IWasFoundOperator.cs
@@ -5,6 +5,7 @@ using R5T.T0132;
 
 namespace R5T.F0000
 {
+    [Obsolete("See D8S.S0015")]
     [FunctionalityMarker]
 	public partial interface IWasFoundOperator : IFunctionalityMarker,
         L0089.IWasFoundOperator 
